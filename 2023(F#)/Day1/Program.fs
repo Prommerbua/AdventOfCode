064bfd34d03a715f57a59dc067cc04e7be99a004
Michael Prommer <p00111452@konzern.work>
2023-12-20 15:25:29 +0100

    Day1 Task1

---
+++ 2023(F#)/Day1/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+let fileName = "../../../example.txt"
+
+let readLines fileName: seq<string> = System.IO.File.ReadLines(fileName)
+
+let lines = readLines(fileName);
+
+let printSeq seq1 = Seq.iter (printf "%A ") seq1; printfn ""
+
+
+let task1 () = 
+    let toNumber (line) = (line
+        |> Seq.filter (fun c -> Char.IsNumber(c)) 
+        |> (fun s -> [|Seq.head s; Seq.last s|]) 
+        |> String.Concat 
+        |> int) 
+
+    let sum = lines |> Seq.map toNumber |> Seq.sum
+    printfn "%d" sum
+
+let task2 () = 
+    let toNumber (line) = (line
+        |> Seq.filter (fun c -> Char.IsNumber(c)) 
+        |> (fun s -> [|Seq.head s; Seq.last s|]) 
+        |> String.Concat 
+        |> int) 
+
+    let sum = lines |> Seq.map toNumber |> Seq.sum
+    printfn "%d" sum
+
+task1()
